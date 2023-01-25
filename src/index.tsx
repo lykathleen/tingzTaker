73a92ea54037cbc0811db0e9767cc02ad1dee8bd
heykathl <heykathl@users.noreply.github.com>
2023-01-25 22:51:56 +0000

    Webpack and ts configurations to successfully compile app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App.tsx"
 
-const container = document.getElementById('app-root')!
-const root = createRoot(container)
-root.render(<App/>)
+reactDom.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '3000',
+    static: {
+      directory: path.join(__dirname, 'public')
+},
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['ts', 'tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, 
+        exclude: /node_modules/, 
+        use: 'babel-loader', 
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
