463a9a73237ccb9589af2fe9e55b226a58b0cd7c
heykathl <heykathl@users.noreply.github.com>
2023-04-22 12:28:51 +0100

    Main component

---
+++ src/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="mainTingEdit">
+        <input type="text" id="title" autoFocus />
+        <textarea id="body" placeholder="Insert your ting" />
+      </div>
+      <div className="mainTingPreview"></div>
+      <h1 className="mainTingPreviewTitle">TITLE</h1>
+      <div className="mainTingMarkdownPreview">Preview</div>
+    </div>
+  );
+};
+
+export default Main;
